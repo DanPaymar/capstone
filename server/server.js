7ef403133af1fcb0f78762a04df430396584e6dc
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-04-05 16:03:32 -0700

    update to server file

--- server/server.js
+++ server/server.js
@@ -5,13 +5,12 @@ const {SERVER_PORT} = process.env
 const app = express();
 
 // Middleware to enable CORS
+app.use(express.static(`$__dirname}/public`))
 app.use(express.json());
 app.use(cors());
 
 // Define a route
-app.get('../public/index.html', (req, res) => {
-    res.send('');
-});
+
 
 // Start the server
 app.listen(SERVER_PORT, () => console.log(`trainspotting on ${SERVER_PORT}`))
