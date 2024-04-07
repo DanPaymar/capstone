584602468fe3e109041578d5034e5f1c9d511df2
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-04-07 16:03:16 -0700

    updated server file

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ app.use(express.static(`$__dirname}/public`))
 app.use(express.json());
 app.use(cors());
 
-// Define a route
+// Define a route to serve the website
+app.get('/', (req, res) => {
+    // send the main HTML file of website
+    // Replace 'index.html' with the actual name of your main HTML file
+    res.sendFile(`${__dirname}/public/index.html`);
+});
 
 
 // Start the server
