ea7e219cd4fc5acf265cf7c666b393b3dfa1cf6d
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-04-11 16:27:33 -0700

    updated login page. updated sign up modal. made formatting and styling fixes to index and created profile template.

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+// const signUpModal = new bootstrap.Modal(document.getElementById('signUpModal'), options)
+
+// // Get the Apple sign-in button element
+// var appleButton = document.getElementById('appleid-signin');
+
+// // Remove the max-width constraint
+// appleButton.style.maxWidth = 'none';
